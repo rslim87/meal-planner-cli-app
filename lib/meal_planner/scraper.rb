d663acfcf4094c96b514675f6609d3148899710d
github username <github email address>
2017-03-07 13:27:40 -0500

    finished scrapping for the information needed

--- lib/meal_planner/scraper.rb
+++ lib/meal_planner/scraper.rb
@@ -1,18 +1,15 @@
 
 
 class Scraper
-	def get_page
-		doc = Nokogiri::HTML(open("https://www.100daysofrealfood.com/real-food-resources/"))
-		doc.css('ol li').each do |list|
-			list.detect {|item| item == "banana"}
-		binding.pry
+# 	def get_page
+# 		doc = Nokogiri::HTML(open("https://www.100daysofrealfood.com/real-food-resources/"))
+# 		doc.css('ol li').each do |list|
+# 			list.detect {|item| item == "banana"}
 		
-	end
-	end
-end
-
-Scraper.new.get_page
-
+		
+# 	end
+# 	end
+# end
 
 
 #breakfast item 1 : doc.css('ol li')[12].text
@@ -27,3 +24,31 @@ Scraper.new.get_page
 #dinner item 2 : doc.css('ol li')[144].text
 #dinner item 3 : doc.css('ol li')[137].text
 
+
+def get_page
+	doc = Nokogiri::HTML(open("https://www.100daysofrealfood.com/recipe-proscuitto-wrapped-scallops/"))
+	binding.pry
+end
+end
+
+Scraper.new.get_page
+
+#Breakfast
+#1. Banana Bread 
+#prep time in minutes: doc.css("span.wprm-recipe-prep_time-minutes").text
+#cook time in minutes: doc.css("span.wprm-recipe-cook_time-minutes").text 
+#ingredients: 
+#step 1 list = doc.css("div.wprm-recipe-ingredient-group").text.gsub("\t", '').gsub("\n", ' ').split("  ")
+#step 2 list.each {|item| puts item}
+#instructions 
+#step 1 ingredients = doc.css("div.wprm-recipe-instruction-group").text.gsub("\t", '').gsub("\n", ' ').gsub("   ","").split(".")
+# step 2 count = 1
+# 		ingredients.each do |item|
+# 		puts "#{count}. #{item}"
+# 		count = count + 1
+# 		end
+
+
+
+
+	
